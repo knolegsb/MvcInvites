cef635330147d90635619c535c5b9bc5c92abf22
knolegsb <seon2you@hotmail.com>
2015-10-26 13:34:33 -0700

    added simple index view

---
+++ MvcInvites/Controllers/InviteController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcInvites.Controllers
+{
+    public class InviteController : Controller
+    {
+        // GET: Invite
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
+            return View();
+        }
+    }
+}
