9a4fd7765ddfc3176e8a972ab6936a832ce4f453
knolegsb <seon2you@hotmail.com>
2015-10-26 15:16:00 -0700

    added style

--- MvcInvites/Controllers/InviteController.cs
+++ MvcInvites/Controllers/InviteController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MvcInvites.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -14,6 +15,25 @@ namespace MvcInvites.Controllers
             int hour = DateTime.Now.Hour;
             ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
             return View();
+        }
+
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            if(ModelState.IsValid)
+            {
+                return View("Thanks", guestResponse);
+            }
+            else
+            {
+                return View();
+            }
         }
     }
 }
