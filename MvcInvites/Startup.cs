32621faf6beeb373a34bb9aa00e32500fde3b5d9
knolegsb <seon2you@hotmail.com>
2015-10-26 13:09:51 -0700

    start

---
+++ MvcInvites/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MvcInvites.Startup))]
+namespace MvcInvites
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
